1003e2702f59d62a1ee740ea043f58ce5ebe12c6
unknown <jpatek@ubiqsecurity.com>
2020-05-16 19:57:50 -0700

    Fixed warning

--- build_common.bat
+++ build_common.bat
@@ -3,6 +3,6 @@ cd common
 git submodule update --init --recursive
 if not exist build mkdir build
 cd build
-cmake ..
+cmake -D CMAKE_BUILD_TYPE=Release ..
 msbuild -verbosity:m INSTALL.vcxproj
 cd ..\..
