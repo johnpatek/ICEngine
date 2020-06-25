6cd6fff64b76b965ca4f35bcb71599a117b69c35
John Patek <johnpatek2@gmail.com>
2020-06-25 15:58:45 -0700

    Putting bluetooth on hold for now to focus on UDP

---
+++ examples/ice-engine/udp_client.cpp
@@ -0,0 +1,11 @@
+#include "ice-engine/argparse.h"
+#include <unistd.h>
+#include <sys/socket.h>
+//#include <bluetooth/bluetooth.h>
+//#include <bluetooth/rfcomm.h>
+
+int main(const int argc, const char ** argv)
+{
+    
+    return 0;
+}
