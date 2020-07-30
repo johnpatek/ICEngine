55d5349694fce037d2a619b86d3c6eb4b0239915
John Patek <johnpatek2@gmail.com>
2020-07-30 03:46:12 -0700

    Initial commit

---
+++ include/ice-engine/threadpool.h
@@ -0,0 +1,13 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+class threadpool
+{
+private:
+    /* data */
+public:
+    threadpool(/* args */);
+    ~threadpool();
+};
+
+#endif
---
+++ src/ice-engine/threadpool.cpp
@@ -0,0 +1,9 @@
+#include "ice-engine/threadpool.h"
+
+ice::threadpool::threadpool(/* args */)
+{
+}
+
+ice::threadpool::~threadpool()
+{
+}
