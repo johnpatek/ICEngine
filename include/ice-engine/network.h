d57a42c52a90006fbdbb2f0fb1177e97b97ae4e6
John Patek <johnpatek2@gmail.com>
2020-07-30 01:48:56 -0700

    Redesign complete

--- include/ice-engine/network.h
+++ include/ice-engine/network.h
@@ -106,11 +106,12 @@ namespace ice
             const std::string& cert_path,
             const std::string& key_path,
             const request_handler_t& request_handler,
-            const uint16_t port);
+            const uint16_t port,
+            const uint32_t threads);
         
         ~tls_server();
 
-        void start(const uint32_t threads);
+        void start();
         
         void run();
         
