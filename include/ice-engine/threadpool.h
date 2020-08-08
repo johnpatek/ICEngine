fd35ffe1332d8792a754a652e750add639de34b1
Max Guerrero <maxguerrero94@gmail.com>
2020-08-08 11:20:49 -0700

    Implemented a first iteration of threadpool: Creates pool, add task to queue  functionality and destruction

--- include/ice-engine/threadpool.h
+++ include/ice-engine/threadpool.h
@@ -1,13 +1,49 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
+//#include "common.h"
+#include <functional>
+#include <iostream>
+#include <future>
+#include <thread>
+#include <queue>
+#include <vector>
+#include <condition_variable>
 
 class threadpool
 {
 private:
     /* data */
+    std::queue<std::function<void()>> work_queue;
+    std::vector<std::thread> worker_threads;
+    std::condition_variable thread_cv;
+    std::mutex cv_mutex;
+    bool mStop;
 public:
-    threadpool(/* args */);
+    threadpool(uint8_t num_threads);
+    template<class F, class... Args>
+    std::future<typename std::result_of<F(Args...)>::type> call_async(F&& f, Args&&...args)
+    {
+       using return_type = typename std::result_of<F(Args...)>::type;
+
+       auto task = std::make_shared<std::packaged_task <return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+       std::future<return_type> result = task->get_future();
+
+       std::function<void()> work([task]()
+       {
+          (*task)();
+       });
+
+       // context for releasing lock after shared value is modified
+       {
+          std::unique_lock<std::mutex> _lock(cv_mutex);
+          work_queue.push(work);
+       }
+
+       thread_cv.notify_all();
+       return result;
+    }
+
+    void run_worker_thread(void);
     ~threadpool();
 };
-
 #endif
--- src/ice-engine/threadpool.cpp
+++ src/ice-engine/threadpool.cpp
@@ -1,9 +1,58 @@
+//#include "common.h"
 #include "ice-engine/threadpool.h"
+#include <functional>
+#include <iostream>
+#include <future>
+#include <thread>
+#include <queue>
+#include <vector>
+#include <condition_variable>
 
-ice::threadpool::threadpool(/* args */)
+threadpool::threadpool(uint8_t num_threads)
 {
+    //
+    worker_threads.reserve(num_threads);
+    // initialize to be running a worker task
+    // worker task should simply use cond variable to access whether or not there is work
+    int i = 0;
+    mStop = false;
+    while(worker_threads.size() < worker_threads.capacity())
+    {
+        worker_threads.push_back(std::thread([this]
+        {
+           this->run_worker_thread();
+	}));
+	worker_threads[i].detach();
+	++i;
+    }
 }
 
-ice::threadpool::~threadpool()
+
+void threadpool::run_worker_thread(void)
 {
+    while(1)
+    {
+        std::function<void()> work_task;
+        {
+            std::unique_lock<std::mutex> _lock(cv_mutex);
+	    std::cout<< "thread_id: "<< std::this_thread::get_id() << std::endl;
+            thread_cv.wait(_lock, [&]{return (!work_queue.empty() || mStop); }); // or stopping
+            if(mStop)
+	    {
+                std::cout<< "exiting thread_id: "<< std::this_thread::get_id() << std::endl;
+                break;
+	    } 
+            std::cout<<"we're here"<<std::endl;
+	    work_task = work_queue.front();
+	    work_queue.pop();
+	}
+        work_task();
+    }
+}
+
+threadpool::~threadpool()
+{
+    std::unique_lock<std::mutex> _lock(cv_mutex);
+    mStop = true;
+    thread_cv.notify_all();
 }
