92ac68c4df79ca1e544d9946a49e9b9c1761a0fd
John Patek <johnpatek2@gmail.com>
2020-08-09 17:05:41 -0700

    Save point

--- include/ice-engine/threadpool.h
+++ include/ice-engine/threadpool.h
@@ -8,6 +8,7 @@
 #include <queue>
 #include <vector>
 #include <condition_variable>
+#include <shared_mutex>
 
 class threadpool
 {
@@ -46,4 +47,67 @@ public:
     void run_worker_thread(void);
     ~threadpool();
 };
+
+
+
+class priority_threadpool
+{
+private:
+
+   using work_t = std::pair<int,std::function<void()>>;
+
+   class compare_work
+   {
+   public:
+      bool operator() (work_t& a, work_t& b)
+      {
+         return a.first < b.first;
+      }
+   };
+
+   std::priority_queue<
+      work_t,
+      std::vector<work_t>,
+      compare_work> _queue;
+   std::shared_timed_mutex _mutex;
+   std::condition_variable_any _cv;
+   bool _stop;
+public:
+   priority_threadpool(size_t pool_size);
+
+   ~priority_threadpool();
+
+   template<class F, class... Args>
+   std::future<typename std::result_of<F(Args...)>::type> call_async(
+      int priority,F&& f, Args&&...args)
+   {
+      std::unique_lock<std::shared_timed_mutex> lock(_mutex, std::defer_lock);
+      using return_type = typename std::result_of<F(Args...)>::type;
+      work_t work;
+
+      work.first = priority;
+
+      auto task = std::make_shared<std::packaged_task <return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+      
+      std::future<return_type> result = task->get_future();
+
+      std::function<void()> function([task]()
+      {
+         (*task)();
+      });
+
+      work.second = function;
+
+      lock.lock();
+      _queue.push(work);
+      lock.unlock();
+      
+      _cv.notify_one();
+
+      return result;
+   }   
+
+   void shutdown();
+};
+
 #endif
